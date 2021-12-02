96dc8bc98218d2e592434628e274ca5fcaa75460
Rémi Berson <remi@brave.com>
2021-12-02 10:46:20 +0100

    Update test Brave version

--- update-brave.sh
+++ update-brave.sh
@@ -2,4 +2,4 @@
 
 rm -fr brave
 mkdir brave
-cd brave && wget 'https://github.com/brave/brave-browser/releases/download/v1.26.74/brave-browser-1.26.74-linux-amd64.zip' -O brave.zip && unzip brave.zip && rm brave.zip
+cd brave && wget 'https://github.com/brave/brave-browser/releases/download/v1.32.113/brave-browser-1.32.113-linux-amd64.zip' -O brave.zip && unzip brave.zip && rm brave.zip
