b2c52bfde081382e0d25e8184a1feffa2644e29c
Yevhen Shymotiuk <yshym@pm.me>
2023-09-12 18:00:13 +0300

    Improve onboarding info and tooling
    - Unify linux and mac start commands
    - Fix insecure patterns development issue
    - Add readme chapter about patterns development

--- update-brave.sh
+++ update-brave.sh
@@ -3,3 +3,4 @@
 rm -fr brave
 mkdir brave
 cd brave && wget 'https://github.com/brave/brave-browser/releases/download/v1.32.113/brave-browser-1.32.113-linux-amd64.zip' -O brave.zip && unzip brave.zip && rm brave.zip
+export BRAVE_PATH="./brave/brave"
