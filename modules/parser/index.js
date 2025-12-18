f118a276eec4d2792b438cc4c8d5754ba3050ad0
Yevhen Shymotiuk <yshym@pm.me>
2025-12-18 20:35:17 +0200

    Add UrlAnalyzer export to `parser`

--- modules/parser/index.js
+++ modules/parser/index.js
@@ -2,3 +2,4 @@ export { ContentExtractor, parseQueryString } from "./content-extractor.js";
 export { isHash } from "./utils/hash-detector.js";
 export { default as Patterns } from "./patterns.js";
 export { checkSuspiciousQuery, sanitizeUrl } from "./utils/sanitizer.js";
+export { default as UrlAnalyzer } from "./url-analyzer.js";
